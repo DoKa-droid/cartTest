d007bd1f2f5c53e26fa82bcfc74c706551e5e401
DoKa-droid <Kate-Kard-2010@ya.ru>
2022-02-17 12:03:54 +0700

    gitignore

--- src/test/java/CartTest.java
+++ src/test/java/CartTest.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.awt.*;
 
 public class CartTest {
